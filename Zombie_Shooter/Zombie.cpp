790115b9cf1707b0d50b3aea8e4bdde8e07ff42a
JamesB2578 <111926253+JamesB2578@users.noreply.github.com>
2022-11-27 12:10:29 -0800

    Update Zombie.cpp

--- Zombie_Shooter/Zombie.cpp
+++ Zombie_Shooter/Zombie.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 void Zombie::spawn(float startX, float startY, int type, int seed)
 {
+
     switch (type)
     {
     case 0:
@@ -38,7 +39,7 @@ void Zombie::spawn(float startX, float startY, int type, int seed)
     // Express this as a fraction of 1
     modifier /= 100; // Now equals between .7 and 1
     m_Speed *= modifier;
-    
+
     // Initialize its location
     m_Position.x = startX;
     m_Position.y = startY;
@@ -46,4 +47,68 @@ void Zombie::spawn(float startX, float startY, int type, int seed)
     m_Sprite.setOrigin(25, 25);
     // Set its position
     m_Sprite.setPosition(m_Position);
+}
+
+bool Zombie::hit()
+{
+    m_Health--;
+    if (m_Health < 0)
+    {
+        // dead
+        m_Alive = false;
+        m_Sprite.setTexture(TextureHolder::GetTexture(
+            "graphics/blood.png"));
+        return true;
+    }
+    // injured but not dead yet
+    return false;
+}
+
+bool Zombie::isAlive()
+{
+    return m_Alive;
+}
+FloatRect Zombie::getPosition()
+{
+    return m_Sprite.getGlobalBounds();
+}
+Sprite Zombie::getSprite()
+{
+    return m_Sprite;
+}
+
+void Zombie::update(float elapsedTime,
+    Vector2f playerLocation)
+{
+    float playerX = playerLocation.x;
+    float playerY = playerLocation.y;
+    // Update the zombie position variables
+    if (playerX > m_Position.x)
+    {
+        m_Position.x = m_Position.x +
+            m_Speed * elapsedTime;
+    }
+    if (playerY > m_Position.y)
+    {
+        m_Position.y = m_Position.y +
+            m_Speed * elapsedTime;
+    }
+
+    if (playerX < m_Position.x)
+    {
+        m_Position.x = m_Position.x -
+            m_Speed * elapsedTime;
+    }
+    if (playerY < m_Position.y)
+    {
+        m_Position.y = m_Position.y -
+            m_Speed * elapsedTime;
+    }
+    // Move the sprite
+    m_Sprite.setPosition(m_Position);
+    // Face the sprite in the correct direction
+    float angle = (atan2(playerY - m_Position.y,
+        playerX - m_Position.x)
+        * 180) / 3.141;
+    m_Sprite.setRotation(angle);
 }
